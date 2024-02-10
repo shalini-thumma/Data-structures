46af7c9ad7aa9efebc9fedf03a4029ba89459af3
shalini-thumma <157685994+shalini-thumma@users.noreply.github.com>
2024-02-10 21:22:49 +0530

    Add files via upload

--- task 2.3.c
+++ task 2.3.c
@@ -12,13 +12,39 @@ int main()
         scanf("%d",&arr[i]);
     }
 
-    int sum;
-    for(int i=0;i<n;i++)
-    {
-        sum+=arr[i];
+    int ele;
+    printf("enter key element: ");
+    scanf("%d",&ele);
 
+int low,high,mid;
+    low=0;
+    high=n-1;
+    mid=(low+high)/2;
+
+    while(low<=high)
+    {
+        if(arr[mid]<ele)
+        {
+            low=mid+1;
+            mid=(low+high)/2;
+            }
+    else if(arr[mid]==ele)
+            {
+                printf("key element %d is found at location %d",ele,mid+1);
+                break;
+
+            }
+            else{
+                high=mid-1;
+                mid=(low+high)/2;
+
+                }
     }
-    printf("\nSum of all elements of the array = %d",sum);
+
+                if(low>high)
+                    printf("key element is not found in the array");
+
+
 
     return 0;
 }
